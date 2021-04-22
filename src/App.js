d82c9cfe1f6b7d49132178dee5ce4aaf2f5d5512
adijain2906 <adijai8800@gmail.com>
2021-04-22 20:15:47 +0530

    Blog maker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import Navbar from './Navbar';
+import Home from './Home';
+import Create from './Create';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            
+            <Route exact path="/">
+              <Home/>
+            </Route>
+           
+            <Route exact path="/create">
+              <Create/>
+            </Route>
+            
+            <Route exact path="/blogs/:id">
+              <BlogDetails/>
+            </Route>
+
+            <Route exact path="*">
+              <NotFound/>
+            </Route>
+
+          </Switch>
+        </div>  
+      </div>
+    </Router>  
   );
+  
 }
 
 export default App;
